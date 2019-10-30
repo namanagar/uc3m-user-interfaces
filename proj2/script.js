7244cc1a666dafb0a54de739eaa0bac43af6078d
Naman Agarwal <naman.agarwal1@duke.edu>
2019-10-31 00:57:59 +0100

    set up basic template

---
+++ proj2/script.js
@@ -0,0 +1,16 @@
+$( function() {
+    $( "ul.drop" ).sortable({
+      connectWith: "ul"
+    });
+ 
+    $( "#list1, #list2, #list3" ).disableSelection();
+  } );
+
+
+$( function() {
+    $( "div.drop" ).sortable({
+      connectWith: "div"
+    });
+ 
+    $( "#biglist1, #biglist2, #biglist3" ).disableSelection();
+  } );
